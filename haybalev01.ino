555d2e5fea9c4173c3a16e70ecff43e31ea8ee50
Andrew Muscha <andrew.muscha@auburn.edu>
2016-09-19 14:32:53 -0500

    Working hourly update

--- haybalev01.ino
+++ haybalev01.ino
@@ -1,7 +1,9 @@
-int CapSense = D7;
-int THSense = D6;
+int CapSense = D3;
+int THSense = D2;
+double CapHalfPeriod = 0;
 double CapPeriod = 0;
 double CapFreq = 0;
+double THHalfPeriod = 0;
 double THPeriod = 0;
 double THFreq = 0;
 String Variable = NULL;
@@ -31,8 +33,8 @@ void setup() {
   }
 
   Particle.publish("Setup complete", NULL, 60, PRIVATE);
-  Particle.variable("Cap Freq", CapFreq);
-  Particle.variable("Temp Freq", THFreq);
+  //Particle.variable("Cap Freq", CapFreq);
+ // Particle.variable("Temp Freq", THFreq);
 
   FirstCheck();
 
@@ -67,28 +69,32 @@ void check_if_hour_elapsed(){
     }else{
       NextHour = CurrentHour + 1;
     }
-    CapPeriod = pulseIn(CapSense, HIGH);
-    CapPeriod = 2 * CapPeriod;
-    THPeriod = pulseIn(THSense, HIGH);
-    THPeriod = 2 * THPeriod;
+    CapHalfPeriod = pulseIn(CapSense, HIGH);
+    CapPeriod = 2 * CapHalfPeriod;
+    CapPeriod = CapPeriod * .000001;
+    THHalfPeriod = pulseIn(THSense, HIGH);
+    THPeriod = 2 * THHalfPeriod;
+    THPeriod = THPeriod * .000001;
   }
 }
 
 void FirstCheck(){
-  CapPeriod = pulseIn(CapSense, HIGH);
-  CapPeriod = 2 * CapPeriod;
-  THPeriod = pulseIn(THSense, HIGH);
-  THPeriod = 2 * THPeriod;
+  CapHalfPeriod = pulseIn(CapSense, HIGH);
+  CapPeriod = 2 * CapHalfPeriod;
+  CapPeriod = CapPeriod * .000001;
+  THHalfPeriod = pulseIn(THSense, HIGH);
+  THPeriod = 2 * THHalfPeriod;
+  THPeriod = THPeriod * .000001;
 
   CapFreq = 1 /  CapPeriod;
   THFreq = 1 / THPeriod;
 
   Variable = String("CapFreq");
-  Data = String(CapPeriod);
+  Data = String(CapFreq);
   Particle.publish(Variable, Data, 60, PRIVATE);
 
   Variable = String("THFreq");
-  Data = String(THPeriod);
+  Data = String(THFreq);
   Particle.publish(Variable, Data, 60, PRIVATE);
 
 }
