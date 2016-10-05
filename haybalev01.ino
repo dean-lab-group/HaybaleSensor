afa7bb57f8ae800d45c9fef4f5e7fec74731cf7c
Andrew Muscha <andrew.muscha@auburn.edu>
2016-10-05 11:58:29 -0500

    Control flow operating correctly

--- haybalev01.ino
+++ haybalev01.ino
@@ -15,139 +15,131 @@
  * email: andrew.muscha@auburn.edu
  * Last Updated: 9/20/2016
  */
-#define HALF_HOUR_IN_SECONDS 1800
-#define SLEEP_TIME_OFFSET 1750
-#define SLEEP_DELAY 5000
+long SleepTimeInSeconds = 86400;
 
+int BattVolt = A5;
 int CapSense = D3;
-int THSense = D2;
-int CtrlLine = D1;
-double CapHalfPeriod = 0;
-double CapPeriod = 0;
+int THSense = D5;
+int CtrlLine = D6;
+int error = D7;
+
+volatile long PulseCount = 0;
+volatile int LoopFlag = 0;
+volatile int PulseFlag = 0;
+
+unsigned long CapLowPeriod = 0;
+unsigned long CapHighPeriod = 0;
 double CapFreq = 0;
-double THHalfPeriod = 0;
-double THPeriod = 0;
+unsigned long THLowPeriod = 0;
+unsigned long THHighPeriod = 0;
 double THFreq = 0;
+double CurrentVoltage = 0;
+
 String Variable = NULL;
 String Data = NULL;
 
-bool PublishStatus = false;
-
-int CurrentHour = 25;
-int NextHour = 25;
-int CurrentMinute = 60;
-int NextMinute = 60;
+unsigned int TimerPeriod = 1000;
 
 int UpdateFlag = 0;
+int SecFlag = 0;
 int FiveMinuteInterval = 30000; //60000 milliseconds in a minute; 300000 in 5
 
-void check_if_half_hour_elapsed();
-void FirstCheck();
 
-Timer timer(FiveMinuteInterval, check_if_half_hour_elapsed); //timer used for periodic checks
+void SetContinueFlag(void);
+
+Timer timer(TimerPeriod, SetContinueFlag);
+
+void SetContinueFlag(){
+  noInterrupts();
+  timer.stop();
+  RGB.color(255,255,0);
+  SecFlag = 1; //set flag
+}
+
+void CapISR(void){
+  RGB.color(0,0,255);
+  //delayMicroseconds(8);
+  //if(pinReadFast(CapSense) == LOW){
+    if(!timer.isActive()){
+      timer.startFromISR();
+    }
+    PulseCount++;
+  //}
+}
 
 void setup() {
+  RGB.control(true);
   pinMode(CapSense, INPUT_PULLDOWN);
   pinMode(THSense, INPUT_PULLDOWN);
   pinMode(CtrlLine, OUTPUT);
-  digitalWrite(CtrlLine, HIGH);
-
-  CurrentHour = Time.hour(); //establish current time
-  if(CurrentHour >= 23){  //if-else statement prevents non-existent hours
-    NextHour = 0;
-  }else{
-    NextHour = CurrentHour + 1;
-  }
-
-  CurrentMinute = Time.minute(); //establish current time
-  if((CurrentMinute + 1) > 59){  //if-else statement prevents non-existent minutes
-    NextMinute = ((CurrentMinute + 1) - 60);
-  }else{
-    NextMinute = CurrentMinute + 1;
-  }
-
-  Particle.variable("Cap Freq", CapFreq); //two cloud variables
-  Particle.variable("Temp Freq", THFreq); //Markus, you can try fetching these using api
-  Particle.variable("stat", PublishStatus);
-
-  FirstCheck(); //initializes current temp and cap values
-
-  Particle.publish("Setup complete", NULL, 60, PRIVATE); //setup function finishes out
-
-  delay(SLEEP_DELAY);
-  System.sleep(HALF_HOUR_IN_SECONDS - SLEEP_TIME_OFFSET);
-  timer.start();
+  pinMode(error, OUTPUT);
+  attachInterrupt(CapSense, CapISR, FALLING);
+  LoopFlag = 1;
+  PulseCount = 0;
+  SecFlag = 0;
 }
 
-void loop() {
-  if(UpdateFlag){  //if values were updated
-    timer.stop(); //prevents timer from intiating more checks while updates are processed
-    UpdateFlag = 0; //clear flag
-    CapFreq = 1 /  CapPeriod; //frequency = 1/periodic
-    THFreq = 1 / THPeriod;
 
-    while(!Particle.connected()){
 
-    }
-    Variable = String("CapFreq"); //preparing data as strings to be published
-    Data = String(CapFreq);
-    Particle.publish(Variable, Data, 60, PRIVATE); //publish current data
+void loop(){
+
+  if(LoopFlag == 1){
+    RGB.color(255,0,0);
+    pinResetFast(CtrlLine);
+    interrupts();
+    LoopFlag = 0;
+  }
+  else if(SecFlag == 1){
+    RGB.color(0,255,0);
+    SecFlag = 0;
+    //  LoopFlag = 1;
+    /*Variable = String("Pulses");
+    Data = String(PulseCount);
+    Particle.publish(Variable, Data, 60, PRIVATE);*/
+
+    CapFreq = PulseCount;//double(PulseCount) / 2.007; //pulses = frequency
+    PulseCount = 0;
+
+    //CapLowPeriod = pulseIn(CapSense, LOW); //pulseIn times a low or high pulse, aka half of a period of a square wave
+    //CapHighPeriod = pulseIn(CapSense, HIGH);
+    delay(1000);
+
+    THLowPeriod = pulseIn(THSense, LOW);
+    THHighPeriod = pulseIn(THSense, HIGH);
+
+    /*CapLowPeriod = pulseIn(CapSense, LOW);
+    CapHighPeriod = pulseIn(CapSense, HIGH);*/
+
+    pinSetFast(CtrlLine); //turn off sensors
+
+
+    THFreq = (1 /  (double(THLowPeriod) + double(THHighPeriod))); //frequency = 1 / period
+    //CapFreq = CapFreq * 1000000;
+    THFreq = THFreq * 1000000;
+
+
+    CurrentVoltage = analogRead(BattVolt);
+    CurrentVoltage = CurrentVoltage * ((3.3*2)/(4095*.914));
+
+    RGB.color(0,255,255);
+    while(!Particle.connected()){ }
+
+    Variable = String("BatteryVolt");
+    Data=String(CurrentVoltage);
+    Particle.publish(Variable, Data, 60, PRIVATE);
 
     Variable = String("THFreq");
     Data = String(THFreq);
     Particle.publish(Variable, Data, 60, PRIVATE);
 
-    delay(SLEEP_DELAY);
-    System.sleep(HALF_HOUR_IN_SECONDS - SLEEP_TIME_OFFSET);
-    timer.start(); //restart timer for periodic updates
+    Variable = String("CapFreq");
+    Data = String(CapFreq);
+    Particle.publish(Variable, Data, 60, PRIVATE);
+    LoopFlag = 1;
+    delay(10000);
+
+
+    RGB.color(0,0,0);
+    System.sleep(SLEEP_MODE_DEEP, 10);
   }
-}
-
-void check_if_half_hour_elapsed(){ //called by timer to check if it's time to update
-  CurrentHour = Time.hour(); //update current time
-  CurrentMinute = Time.minute();
-  //if(CurrentHour == NextHour){ //check if hour elapsed and set flag if it has
-  if(CurrentMinute == NextMinute){
-    UpdateFlag = 1;
-    if((CurrentMinute + 1) > 59){  //if-else statement prevents non-existent minutes
-      NextMinute = ((CurrentMinute + 1) - 60);
-    }else{
-      NextMinute = CurrentMinute + 1;
-    }
-    digitalWrite(CtrlLine, LOW);
-    CapHalfPeriod = pulseIn(CapSense, HIGH); //pulseIn times a low or high pulse, aka half of a period of a square wave
-    CapPeriod = 2 * CapHalfPeriod; //double pulse length
-    CapPeriod = CapPeriod * .000001; //pulseIn returns value in milliseconds; convert to seconds
-    THHalfPeriod = pulseIn(THSense, HIGH);
-    THPeriod = 2 * THHalfPeriod;
-    THPeriod = THPeriod * .000001;
-    digitalWrite(CtrlLine, HIGH);
-  }
-}
-
-void FirstCheck(){
-
-  digitalWrite(CtrlLine, LOW);
-  delayMicroseconds(15);
-  CapHalfPeriod = pulseIn(CapSense, HIGH); //pulseIn times a low or high pulse, aka half of a period of a square wave
-  CapPeriod = 2 * CapHalfPeriod; //double pulse length
-  CapPeriod = CapPeriod * .000001; //pulseIn returns value in milliseconds; convert to seconds
-  THHalfPeriod = pulseIn(THSense, HIGH);
-  THPeriod = 2 * THHalfPeriod;
-  THPeriod = THPeriod * .000001;
-  digitalWrite(CtrlLine, HIGH);
-
-  CapFreq = 1 /  CapPeriod; //frequency = 1 / period
-  THFreq = 1 / THPeriod;
-
-  Variable = String("CapFreq"); //preparing data as strings to be published
-  Data = String(CapFreq);
-  PublishStatus = Particle.publish(Variable, Data, 60, PRIVATE); //publish current data
-
-  Variable = String("THFreq");
-  Data = String(THFreq);
-  PublishStatus = Particle.publish(Variable, Data, 60, PRIVATE);
-
-
-
 }
